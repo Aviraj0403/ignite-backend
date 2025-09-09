63dc93b63b5a3ef58ac5ba94189a5d32e9d9f905
aviraj0403 <aviraj0403@gmail.com>
2025-09-10 00:51:32 +0530

    refactor: update project name to ignite-backend and adjust MongoDB connection

--- video-service/app.js
+++ video-service/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import connectDB from './config/dbConfig.js';
 import courseRoutes from './routes/course.routes.js';
 
 dotenv.config();
@@ -12,11 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('📦 MongoDB connected for Video Service'));
-
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('📦 MongoDB connected for Video Service'));
+connectDB()
+  .then(() => {
+    console.log("✅ MongoDB connected for Video Service");
+  })
+  .catch((err) => {
+    console.error("MongoDB  Video Service failed:", err);
+  });
 app.use('/api/standalone-courses', courseRoutes);
 
 export default app;
--- video-service/index.js
+++ video-service/index.js
@@ -1,7 +1,7 @@
 // video-service/index.js
 import app from './app.js';
-const PORT = process.env.PORT || 5006;
-
+// const PORT = process.env.video.PORT1 || 5006;
+const PORT =  6002;
 app.listen(PORT, () => {
   console.log(`🎥 Video Service running on port ${PORT}`);
 });
