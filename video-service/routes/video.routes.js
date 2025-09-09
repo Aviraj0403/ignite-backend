058f59741a002ef15f25bd9d41454b2974a0a656
aviraj0403 <aviraj0403@gmail.com>
2025-09-10 01:18:13 +0530

    add

--- video-service/routes/video.routes.js
+++ video-service/routes/video.routes.js
@@ -4,6 +4,12 @@ import { markProgress } from '../controllers/course.controller.js';
 
 const router = express.Router();
 
+// Define a route for the Course API
+router.get('/', (req, res) => {
+  res.send('microservice is running');
+});
+
+// Define a route to mark course progress
 router.post('/progress', markProgress);
 
 export default router;
